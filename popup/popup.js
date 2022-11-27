ced6ae3a1680f276d5fec84a8ed21f3d81d27d3e
Marios Isaakidis <prometheas@autistici.org>
2022-11-27 12:07:42 +0200

    WIP: Check active policy radio in popup

--- popup/popup.js
+++ popup/popup.js
@@ -1,12 +1,27 @@
+function setChecked() {
+    chrome.storage.local.get(["defaultPolicy"], (defaultPolicy) => {
+        const selectedRadio = document.getElementById("RADIO:" + defaultPolicy);
+        if (selectedRadio === null) {
+            return;
+        }
+        selectedRadio.setAttribute("checked", true);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    setChecked();
+});
+
 async function submittedDefaultPolicy(defaultPolicy) {
     chrome.storage.local.set({"defaultPolicy": defaultPolicy}, () => {
         console.log("Default Policy set to " + defaultPolicy);
     });
 }
 
-var defaultPolicyForm = document.getElementById("defaultPolicyForm");
+const defaultPolicyForm = document.getElementById("defaultPolicyForm");
 defaultPolicyForm.onsubmit = (event) => {
     const formData = new FormData(event.target);
     const defaultPolicy = formData.get("defaultPolicy");
     submittedDefaultPolicy(defaultPolicy);
+    window.close();
 }
