5c17c58298ca988e7ff60f0754887e26a33ef843
Marios Isaakidis <prometheas@autistici.org>
2022-11-27 14:57:34 +0200

    Stylize the response of the mock server

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const requestListener = function (req, res) {
     const DNT = req.headers.dnt;
 
     res.writeHead(200);
-    res.end("DNT: " + DNT || "unset");
+
+    res.end("<h1 style='text-align: center; height: 100vh; background: chartreuse; display: flex; justify-content: center; align-items: center;'>DNT: " +
+        DNT || "unset" + "</h1>");
 };
 
 const server = http.createServer(requestListener);
